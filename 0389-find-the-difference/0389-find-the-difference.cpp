7b66234df930e80339cd6efd7e6b217927b26d74
Tarun Gupta <tarungupta854597@gmail.com>
2026-08-20 16:20:26 +0530

    Time: 0 ms (100%), Space: 9.3 MB (51.97%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int arr[26]={0};
+        for(int i=0;i<s.size();i++)
+        {
+            arr[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            arr[t[i]-'a']--;
+        }
+        char result;
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]==-1)
+            {
+                return i+'a';
+            }
+        }
+        return ' ';
+        
+    }
+};
