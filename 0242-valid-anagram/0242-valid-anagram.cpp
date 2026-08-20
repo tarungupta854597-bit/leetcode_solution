cd7cbea1f4231dbbbc8803be9e075cbbdbd5637f
Tarun Gupta <tarungupta854597@gmail.com>
2026-08-20 15:18:10 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.93%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr[26]={0};
+        for(int i=0;i<s.size();i++)
+        {
+            arr[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            arr[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
