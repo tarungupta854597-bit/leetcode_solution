743a1405775d8bb8ad6eac799cd68a2043997ae3
Tarun Gupta <tarungupta854597@gmail.com>
2026-09-05 17:06:19 +0530

    Time: 0 ms (100%), Space: 100.3 MB (92.76%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -9,10 +9,7 @@ public:
         {
             inc=false;
         }
-    }
-    for(int i=0;i<nums.size()-1;i++)
-    {
-        if(nums[i]<nums[i+1])
+        else if(nums[i]<nums[i+1])
         {
             dec=false;
         }
